75859960b9f9bf0c96c16703b23d354a1e457f80
Tue(Dolphin) Tran <tuetamtran2307@gmail.com>
2026-07-31 14:47:26 +0700

    FIXED: config with pydantic-settings

--- system/config.py
+++ system/config.py
@@ -6,34 +6,31 @@ Goals:
 
 """
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from dataclasses import dataclass #a decorator that examines a class to find a field
 
-@dataclass
+
 class Settings(BaseSettings): 
 
     #Project Information
-    PROJECT_NAME = "SANDI"
+    PROJECT_NAME: str = "SANDI"
 
     #Models: What model objects to use? 
-    LANGUAGE_MODEL = "qwen3:8b"  
-    EMBEDDING_MODEL = "BAAI/bge-m3" 
+    LANGUAGE_MODEL: str = "qwen3:8b"  
+    EMBEDDING_MODEL: str = "BAAI/bge-m3" 
 
     #RAG
-    TOP_K = 5 #Number of relavant text chunks to be trieved from database
-    CHUNK_SIZE = 500 #Max number of units into a single text segment
-    CHUNK_OVERLAP = 75 #Max number of units consecutive chunks share - preventing loss of data
+    TOP_K: int = 5 #Number of relavant text chunks to be trieved from database
+    CHUNK_SIZE: int = 500 #Max number of units into a single text segment
+    CHUNK_OVERLAP: int = 75 #Max number of units consecutive chunks share - preventing loss of data
 
     #Data Directory
-    RAW_DATA_DIR = "data/raw"
-    PROCESSED_DATA_DIR = "data/processed"
+    RAW_DATA_DIR: str = "data/raw"
+    PROCESSED_DATA_DIR: str = "data/processed"
 
     #Vector DB
-    VECTOR_DB_DIR = "rag/vector_db"
+    VECTOR_DB_DIR: str = "rag/vector_db"
 
     #Development
-    OLLAMA_HOST = "http://localhost:11434"
-
-
+    OLLAMA_HOST: str = "http://localhost:11434"
 
 
 settings = Settings() #Setting object to be called
